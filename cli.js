517106ecc92fc9a866b6d3f384128fdef76385ae
Christal Vigil <c.vigil27@gmail.com>
2020-08-21 22:27:04 -0600

    trigger request for prompts and store as variables

--- cli.js
+++ cli.js
@@ -1,80 +1,96 @@
-//Homework: no front end, run node first and answer questions. Use inquire prompt to answer questions, take that data and pass data into function that will return something back to us in the form of a markdown file 
+//import everything that we need for npm 
 
 const inquirer= require('inquirer'); 
 console.log("Inquirer", inquirer);
-
 const fs= require('fs'); 
-const { title } = require('process');
+const util = require('util'); 
+// const { title } = require('process');
 console.log("fs", fs);
+console.log("util", util); 
 
-inquirer.prompt ([
-    {
-        type: "input", 
-        name: "title", 
-        message: "What is the title of your project"
-    }, 
-    {
-        type: "input", 
-        name: "description", 
-        message: "Description of project"
-    },
-    {
-        type: "input", 
-        name: "table of contents", 
-        message: "table of contents"
-    },
-    {
-        type: "input", 
-        name: "installation", 
-        message: "Installation Instructions"
-    },
-    {
-        type: "input", 
-        name: "Usage", 
-        message: "Usage Information"
-    },
-    {
-        type: "input", 
-        name: "Contributing", 
-        message: "Contributing Guidelines"
-    },
-    {
-        type: "input", 
-        name: "test", 
-        message: "Test Instructions"
-    },
-    {
-        type: "input", 
-        name: "questions", 
-        message: "Questions"
+//create a promise instead of a callback function to handle asynchronous behavior 
+//promisify is built into the util 
+const writeFileAsync = util.promisify(fs.writeFile); 
+
+const promptUser = () => {
+    return inquirer.prompt([ 
+        {
+            type: "input", 
+            name: "title", 
+            message: "What is the title of your project"
+        }, 
+        {
+            type: "input", 
+            name: "description", 
+            message: "Description of project"
+        },
+        {
+            type: "input", 
+            name: "table", 
+            message: "table of contents"
+        },
+        {
+            type: "input", 
+            name: "installation", 
+            message: "Installation Instructions"
+        },
+        {
+            type: "input", 
+            name: "Usage", 
+            message: "Usage Information"
+        },
+        {
+            type: "list", 
+            name: "License", 
+            message: "What kind of license should your project have?",
+            choices: [
+                "APACHE 2.8",
+                "GPL 3.0",
+                "BSD 3",
+                "NONE"
+            ]
+        },
+        {
+            type: "input", 
+            name: "contributing", 
+            message: "Contributing Guidelines"
+        },
+        {
+            type: "input", 
+            name: "test", 
+            message: "Test Instructions"
+        },
+        {
+            type: "input", 
+            name: "questions", 
+            message: "Questions"
+        }
+    ]); 
+}
+
+//string template literals for generating a string version 
+const generateREADME = ({title, description, table, installation, usage, license, contributing, test, questions}) => {
+    // return '
+    // README'; 
+}
+
+//trigger the request for the prompt and store as a variable and run in an async way 
+const init = async () => {
+    console.log('starting prompts');
+    try {
+        //store answers to prompts 
+        const answers = await promptUser(); 
+        //stored readme in variable 
+        const readme = generateREADME(answers); 
+        //write file (call function becauase we alredy wrote the file) once readme is generated 
+        
+        writeFileAsync('readme', readme);
+        console.log('successfully wrote to readme.md');
+    } catch (error) {
+        console.log(error); 
     }
-])
-
-// ]).then(function(data) {
-
-//     var filename = data.name.toLowerCase().split(' ').join('') + ".json";
-  
-//     fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
-  
-//       if (err) {
-//         return console.log(err);
-//       }
-  
-//       console.log("Success!");
-  
-//     });
-//   });
+}; 
+    init(); 
 
 
- // Turn the arrays into JSON strings so they can be written to files
-//  const dogJSON = JSON.stringify(dogs, null, 2);
-//  const catJSON = JSON.stringify(cats, null, 2);
 
-//  writeFileAsync("dogs.json", dogJSON).then(function() {
-//    console.log("Successfully wrote to dogs.json file");
-//  });
-
-//  writeFileAsync("cats.json", catJSON).then(function() {
-//    console.log("Successfully wrote to cats.json file");
-//  });
-// });
